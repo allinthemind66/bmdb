9d55c6191efc7871a09be34aca919b636b1ce45b
allinthemind66 <r.nilon92@gmail.com>
2021-12-16 23:31:16 -0500

    add links to hall of shame people

--- src/components/hall-of-shame.js
+++ src/components/hall-of-shame.js
@@ -13,64 +13,65 @@ export const HallOfShame = () => {
             </Row>
             <Row>
                 <Col>
-                    <img
-                        className="d-block w-100"
-                        src={require("../images/robertZdar.jpg")}
-                        alt="Second slide"
-                    />
-                    <a href="#">Robert Z'Dar</a></Col>
-                <Col><img
+                    <a href="#">
+                        <img
+                            className="d-block w-100"
+                            src={require("../images/robertZdar.jpg")}
+                            alt="Second slide"
+                        />
+                        Robert Z'Dar</a></Col>
+                <Col><a href="#"><img
                     className="d-block w-100"
                     src={require("../images/joeEstevez.jpg")}
                     alt="Second slide"
-                /><a href="#">Joe Estevez</a></Col>
-                <Col><img
+                />Joe Estevez</a></Col>
+                <Col><a href="#"><img
                     className="d-block w-100"
                     src={require("../images/paulyShore.jpg")}
                     alt="Second slide"
-                /><a href="#">Pauly Shore</a></Col>
-                <Col>
+                />Pauly Shore</a></Col>
+                <Col><a href="#">
                     <img
                         className="d-block w-100"
                         src={require("../images/Tommy-Wiseau-1.jpeg")}
                         alt="Second slide"
-                    /><a href="#">Tommy Wiseau</a></Col>
-                <Col> <img
+                    />Tommy Wiseau</a></Col>
+                <Col> <a href="#"><img
                     className="d-block w-100"
                     src={require("../images/kimCoppola.jpg")}
                     alt="Second slide"
-                /><a href="#">Nicolas Cage</a></Col>
+                />Nicolas Cage</a></Col>
 
             </Row>
             <Row>
                 <Col><h3 class="hallOfShame"> Directors</h3></Col>
             </Row>
             <Row>
-                <Col><img
+                <Col><a href="#"><img
                     className="d-block w-100"
                     src={require("../images/james.jpg")}
                     alt="Second slide"
-                /><a href="#">James Nguyen</a></Col>
-                <Col><img
+                />James Nguyen</a></Col>
+                <Col><a href="#"><img
                     className="d-block w-100"
                     src={require("../images/Tommy-Wiseau-1.jpeg")}
                     alt="Second slide"
-                /><a href="#">Tommy Wiseau</a></Col>
-                <Col><img
+                />Tommy Wiseau</a></Col>
+                <Col><a href="#"><img
                     className="d-block w-100"
                     src={require("../images/edWood.jpg")}
                     alt="Second slide"
-                /><a href="#">Ed Wood</a></Col>
-                <Col><img
+                />Ed Wood</a></Col>
+                <Col><a href="#"><img
                     className="d-block w-100"
                     src={require("../images/joelSchumacher.jpg")}
                     alt="Second slide"
-                /><a href="#">Joel Schumacher</a></Col>
-                <Col><img
+                />Joel Schumacher</a></Col>
+                <Col><a href="#"><img
                     className="d-block w-100"
                     src={require("../images/schamylan.jpg")}
                     alt="Second slide"
-                /><a href="#">M. Night Shyamalan</a></Col>
+                />M. Night Shyamalan</a></Col>
             </Row>
         </>
     )
--- src/components/recent-movies.js
+++ src/components/recent-movies.js
@@ -5,39 +5,46 @@ export const RecentMovies = () => {
     return (
         <Carousel>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require("../images/goneFishing.jpg")}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>Gone Fishing</h3>
-                    <p>Two fishing fanatics get in trouble when their car is stolen during a fishing trip.</p>
-                </Carousel.Caption>
+                <a href="#">
+
+                    <img
+                        className="d-block w-100"
+                        src={require("../images/goneFishing.jpg")}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Gone Fishing</h3>
+                        <p>Two fishing fanatics get in trouble when their car is stolen during a fishing trip.</p>
+                    </Carousel.Caption>
+                </a>
             </Carousel.Item>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require("../images/notMyKid.jpg")}
-                    alt="Second slide"
-                />
+                <a href="#">
+                    <img
+                        className="d-block w-100"
+                        src={require("../images/notMyKid.jpg")}
+                        alt="Second slide"
+                    />
 
-                <Carousel.Caption>
-                    <h3>Not My Kid</h3>
-                    <p>A young drug-addicted teenager's family are in denial about it. She's sent to an intervention program where the addicts are confronted--by one another and by their families--in supervised ...</p>
-                </Carousel.Caption>
+                    <Carousel.Caption>
+                        <h3>Not My Kid</h3>
+                        <p>A young drug-addicted teenager's family are in denial about it. She's sent to an intervention program where the addicts are confronted--by one another and by their families--in supervised ...</p>
+                    </Carousel.Caption>
+                </a>
             </Carousel.Item>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={require("../images/raidersOfTheSun.jpg")}
-                    alt="Third slide"
-                />
+                <a href="#">
+                    <img
+                        className="d-block w-100"
+                        src={require("../images/raidersOfTheSun.jpg")}
+                        alt="Third slide"
+                    />
 
-                <Carousel.Caption>
-                    <h3>Raiders Of The Sun</h3>
-                    <p>In a post-apocalyptic future, a commando named Brodie must defend the only village that still has gunpowder from a ruthless gang of raiders led by Clay, a traitor who plans to use the deadly powder to conquer the democratic Alpha League.</p>
-                </Carousel.Caption>
+                    <Carousel.Caption>
+                        <h3>Raiders Of The Sun</h3>
+                        <p>In a post-apocalyptic future, a commando named Brodie must defend the only village that still has gunpowder from a ruthless gang of raiders led by Clay, a traitor who plans to use the deadly powder to conquer the democratic Alpha League.</p>
+                    </Carousel.Caption>
+                </a>
             </Carousel.Item>
         </Carousel>
     )
